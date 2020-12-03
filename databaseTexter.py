a1a622acb3baa1f7575b715d4b0a4cc8a3764c9d
Austin <austinbarron42@gmail.com>
2020-12-03 11:56:13 -0600

    updated comments

--- databaseTexter.py
+++ databaseTexter.py
@@ -2,30 +2,34 @@
 
 # Note: For the from variable. This number may change but will be hard coded for demonstration.
 
-from twilio.rest import Client
-import pymysql
+from twilio.rest import Client ##imports API for twilio
+import pymysql ##imports module for mysql database manipulation
 
-account_sid = 'AC9a765b751eb35584336004c8fc5382b8'
-auth_token = 'db06dd600d5bdac989bb9f16f8e02172'
-client = Client(account_sid, auth_token)
+account_sid = 'AC9a765b751eb35584336004c8fc5382b8' ##account id for twilio
+auth_token = 'db06dd600d5bdac989bb9f16f8e02172' ##account token for twilio
+client = Client(account_sid, auth_token) ##client object declaration
 
-db = pymysql.connect("localhost", "testuser", "test123", "persons")
+db = pymysql.connect("localhost", "testuser", "test123", "persons") ##database connection established with sample user
 
-cursor = db.cursor()
+cursor = db.cursor() ##cursor object declaration
 
 sql = "SELECT * FROM testcase \
-    WHERE ZIP_CODE = '%s'" % ('60401')
-
+    WHERE ZIP_CODE = '%s'" % ('60401') ##sql query for finding a desired zipcode to text to
+##try will attempt to execute the sql query, except will print an error if it cannot.
 try:
-    cursor.execute(sql)
+    cursor.execute(sql) ##executes the sql query 'sql'
 
-    results = cursor.fetchall()
+    results = cursor.fetchall() ##fetches all results from sql query
+
+    #for in that parses each entry from results and assigns them to variables
     for row in results:
         idenNum = row[0]
         fullName = row[1]
         phoneNum = row[2]
         zip_code = row[3]
 
+        ##client will create a new text message containing a predesignated message
+        ##and send that message to the given phonenumber from the twilio bot.
         message = client.messages \
                   .create(
                       body='This is A Test Please Disregard <3',
